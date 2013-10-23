34da0159b3292eab25def0d49d9d720a217edc57
Monfee <eric.tang2005@gmail.com>
2013-10-23 11:27:25 +1000

    still working on the jni for atire

--- java/AtireJ/src/org/atire/Atire.java
+++ java/AtireJ/src/org/atire/Atire.java
@@ -1,9 +1,16 @@
 package org.atire;
 
-public class Atire {
-	
-	public void startServer() {
-		
-	}
+import org.atire.swig.atire_apis;
 
+public class Atire {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			atire_apis.run_atire("-q:");
+		}
+		atire_apis.run_atire(args[0]);
+	}
 }
