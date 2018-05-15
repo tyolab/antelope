f715f5b5d3908ca383065f06ef2e3f0a1e8345d2
Eric Tang <eric.tang@gmail.com>
2018-05-15 17:27:28 +1000

    add missing files

---
+++ source/channel_memory.c
@@ -0,0 +1,61 @@
+/*
+	CHANNEL_FILE.C
+	--------------
+*/
+#include "channel_memory.h"
+
+/*
+	ANT_CHANNEL_FILE::ANT_CHANNEL_FILE()
+	------------------------------------
+	if filename == NULL then use stdin and stout else use the named file
+*/
+ANT_channel_memory::ANT_channel_memory(char *filename)
+{
+memory = new char[(memory_size)];
+}
+
+/*
+	ANT_CHANNEL_FILE::~ANT_CHANNEL_FILE()
+	-------------------------------------
+*/
+ANT_channel_memory::~ANT_channel_memory()
+{
+delete [] memory;
+}
+
+/*
+	ANT_CHANNEL_FILE::BLOCK_WRITE()
+	-------------------------------
+*/
+long long ANT_channel_memory::block_write(char *source, long long length)
+{
+memcpy(memory + position, source, length);
+position += length;
+return length;
+}
+
+/*
+	ANT_CHANNEL_FILE::BLOCK_READ()
+	------------------------------
+*/
+char *ANT_channel_memory::block_read(char *into, long long length)
+{
+long max_len = length > memory_size ? memory_size : length;
+memcpy(into, memory, max_len);
+return NULL;
+}
+
+/*
+	ANT_CHANNEL_FILE::GETSZ()
+	-------------------------
+*/
+char *ANT_channel_memory::getsz(char terminator)
+{
+long length = position + 1;
+memory[length] = terminator;
+// char *buffer = new char[lenght];
+// memcpy();
+position = 0;
+return memory;
+}
+
---
+++ source/channel_memory.h
@@ -0,0 +1,37 @@
+/*
+	CHANNEL_MEMORY.H
+	--------------
+*/
+#ifndef CHANNEL_MEMORY_H_
+#define CHANNEL_MEMORY_H_
+
+#include <stdio.h>
+#include "channel.h"
+
+class ANT_instream;
+class ANT_memory;
+
+/*
+	class ANT_CHANNEL_MEMORY
+	----------------------
+*/
+class ANT_channel_memory : public ANT_channel
+{
+protected:
+	static const long memory_size = (16 * 1024);
+
+private:
+	char *memory;
+	long position;
+	
+protected:
+	virtual long long block_write(char *source, long long length);
+	virtual char *block_read(char *into, long long length);
+	virtual char *getsz(char terminator = '\0');
+
+public:
+	ANT_channel_memory(char *filename = NULL);
+	virtual ~ANT_channel_memory();
+} ;
+
+#endif /* CHANNEL_MEMORY_H_ */
